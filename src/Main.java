27b6683fa558eeaceab4f6bfb0c1593b06d097e6
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-06-24 15:26:17 +0300

    done task5

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        MonitoringSystem generalIndicatorsMonitoringModule = new MonitoringSystem() {
+            @Override
+            public void startMonitoring() {
+                System.out.println("Monitoring of general indicators has started!");
+            }
+        };
+
+        MonitoringSystem errorMonitoringModule = new MonitoringSystem() {
+            @Override
+            public void startMonitoring() {
+                System.out.println("Bug tracking monitoring has started!");
+            }
+        };
+
+        MonitoringSystem securityModule = new MonitoringSystem() {
+            @Override
+            public void startMonitoring() {
+                System.out.println("Security monitoring has started!");
+            }
+        };
+
+        generalIndicatorsMonitoringModule.startMonitoring();
+        errorMonitoringModule.startMonitoring();
+        securityModule.startMonitoring();
+    }
+}
