20bf303f07e46b18718521910016154d4c0bd112
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-06-27 08:36:18 +0300

    merge two main() functions to one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
+
+        String example = "Hello, user! How are you?";
+
+        String result = null;
+
+        result = TextProcessor.appendText(example, "Fine!");
+        System.out.println(result);
+
+        result = TextProcessor.insertText(example, "dear", 6);
+        System.out.println(result);
+
+        result = TextProcessor.removeCharacter(example, '!');
+        System.out.println(result);
+
+        result = TextProcessor.replaceSubstring(example, "user", "programmer");
+        System.out.println(result);
+
+        TextProcessor.readAndReverse();
+
+        result = TextProcessor.readAndDeleteSpaces();
+        System.out.println(result);
+
+        result = TextProcessor.outputUppercase("YY d dI S P AAAa aa AAA aaA");
+        System.out.println(result);
+
+        result = TextProcessor.uppercaseEvenLetters("Friends");
+        System.out.println(result);
+
+        String[] array = TextProcessor.readAndEditStringArray();
+        for (String s:
+                array) {
+            System.out.println(s);
+        }
+
         String str1 = "Java";
         String str2 = "JAVA";
         String str3 = "C#";
