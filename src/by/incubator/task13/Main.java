4bc85bd2fcc64a13095ddd2df39d694dc7ca170f
ElizavetaSergeeva-Nekrasova <106116169+ElizavetaSergeeva-Nekrasova@users.noreply.github.com>
2022-07-16 12:28:01 +0300

    Update Main.java

--- src/by/incubator/task13/Main.java
+++ src/by/incubator/task13/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> myArrayList = new MyArrayList<>();
         MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(JosephProblemWithArrayList.getLastSurvivorNumber(myArrayList));
-        System.out.println(JosephProblemWithArrayList.getLastSurvivorNumber(myLinkedList));
+        System.out.println(JosephProblemWithArrayList.getLastSurvivorNumber(myArrayList, scanner));
+        System.out.println(JosephProblemWithArrayList.getLastSurvivorNumber(myLinkedList, scanner));
     }
 }
