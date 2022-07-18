5cfd2246305c1bffe943acdd2c46f7157a128961
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-07-18 11:10:28 +0300

    write MyHashSet class

--- src/by/incubator/task14/Main.java
+++ src/by/incubator/task14/Main.java
@@ -2,9 +2,7 @@ package by.incubator.task14;
 
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
-        myHashMap.put("Hello", 7);
-        myHashMap.put("Hello", 5);
-        System.out.println(myHashMap.get("Hello"));
+        private MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+
     }
 }
---
+++ src/by/incubator/task14/MyHashSet.java
@@ -0,0 +1,81 @@
+package by.incubator.task14;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MyHashSet<K> implements Set<K> {
+    private HashMap<K, Object> hashMap;
+    private static final Object STOPPER = new Object();
+
+    public MyHashSet() {
+        hashMap = new HashMap<>();
+    }
+
+    @Override
+    public int size() {
+        return hashMap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean add(K k) {
+        return hashMap.put(k, STOPPER) == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+
+        return hashMap.remove(o) == STOPPER;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends K> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return hashMap.containsKey(o);
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+}
