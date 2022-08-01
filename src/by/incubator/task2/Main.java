23c9626f564f30e4b701809bddeb2669f7fdaa60
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-08-01 19:14:05 +0300

    update task2

--- src/by/incubator/task2/Main.java
+++ src/by/incubator/task2/Main.java
@@ -2,6 +2,6 @@ package by.incubator.task2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from jar!");
+        System.out.println("Hello from jar!!!");
     }
 }
